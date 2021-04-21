5ccdcf45acd9dfbef5996d5875f2ba93b7495bad
Antonis Ortiz <aortizv@uniguajira.edu.co>
2021-04-20 19:00:53 -0500

    subiendo proyecto de login con Api fake sencillo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,85 +5,106 @@ import axios from 'axios';
 import md5 from 'md5';
 import Cookies from 'universal-cookie';
 
-const baseUrl="http://localhost:3001/usuarios";
+const baseUrl = 'http://localhost:3001/Usuarios';
 const cookies = new Cookies();
 
 class Login extends Component {
-    state={
-        form:{
+    state = {
+        form: {
             username: '',
-            password: ''
-        }
-    }
+            password: '',
+        },
+    };
 
-    handleChange=async e=>{
+    handleChange = async (e) => {
         await this.setState({
-            form:{
+            form: {
                 ...this.state.form,
-                [e.target.name]: e.target.value
-            }
+                [e.target.name]: e.target.value,
+            },
         });
-    }
+    };
 
-    iniciarSesion=async()=>{
-        await axios.get(baseUrl, {params: {username: this.state.form.username, password: md5(this.state.form.password)}})
-        .then(response=>{
-            return response.data;
-        })
-        .then(response=>{
-            if(response.length>0){
-                var respuesta=response[0];
-                cookies.set('id', respuesta.id, {path: "/"});
-                cookies.set('apellido_paterno', respuesta.apellido_paterno, {path: "/"});
-                cookies.set('apellido_materno', respuesta.apellido_materno, {path: "/"});
-                cookies.set('nombre', respuesta.nombre, {path: "/"});
-                cookies.set('username', respuesta.username, {path: "/"});
-                alert(`Bienvenido ${respuesta.nombre} ${respuesta.apellido_paterno}`);
-                window.location.href="./menu";
-            }else{
-                alert('El usuario o la contraseña no son correctos');
-            }
-        })
-        .catch(error=>{
-            console.log(error);
-        })
+    iniciarSesion = async () => {
+        await axios
+            .get(baseUrl, {
+                params: {
+                    username: this.state.form.username,
+                    password: md5(this.state.form.password),
+                },
+            })
+            .then((response) => {
+                return response.data;
+            })
+            .then((response) => {
+                if (response.length > 0) {
+                    var respuesta = response[0];
+                    cookies.set('id', respuesta.id, {
+                        path: '/',
+                    });
+                    cookies.set('primer_apellido', respuesta.primer_apellido, {
+                        path: '/',
+                    });
+                    cookies.set('segundo_apellido', respuesta.segundo_apellido, {
+                        path: '/',
+                    });
+                    cookies.set('nombre', respuesta.nombre, {
+                        path: '/',
+                    });
+                    cookies.set('genero', response.genero, {
+                        path: '/',
+                    });
 
-    }
+                    cookies.set('username', respuesta.username, {
+                        path: '/',
+                    });
+                    alert(`Bienvenido ${respuesta.nombre} ${respuesta.primer_apellido}`);
+                    window.location.href = './menu';
+                } else {
+                    alert('El usuario o la contraseña no son correctos');
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     componentDidMount() {
-        if(cookies.get('username')){
-            window.location.href="./menu";
+        if (cookies.get('username')) {
+            window.location.href = './menu';
         }
     }
-    
 
     render() {
         return (
-    <div className="containerPrincipal">
-        <div className="containerSecundario">
-          <div className="form-group">
-            <label>Usuario: </label>
-            <br />
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>Contraseña: </label>
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              onChange={this.handleChange}
-            />
-            <br />
-            <button className="btn btn-primary" onClick={()=> this.iniciarSesion()}>Iniciar Sesión</button>
-          </div>
-        </div>
-      </div>
+            <div className="container_1">
+                <div className="container_2">
+                    <div className="form-group">
+                        <label> Nombre de Usuario </label> <br />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="username"
+                            onChange={this.handleChange}
+                        />{' '}
+                        <br />
+                        <label> contraseña </label> <br />
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            onChange={this.handleChange}
+                        />{' '}
+                        <br />
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => this.iniciarSesion()}
+                        >
+                            Iniciar Sesión
+                        </button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,39 +1,131 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import Cookies from 'universal-cookie';
+import { NavDropdown } from 'react-bootstrap';
+// import { NavbarBrand } from 'react-bootstrap';
+// import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+// import ListGroup from 'react-bootstrap/ListGroup';
+
 
 const cookies = new Cookies();
 
 class Menu extends Component {
-    cerrarSesion=()=>{
-        cookies.remove('id', {path: "/"});
-        cookies.remove('apellido_paterno', {path: "/"});
-        cookies.remove('apellido_materno', {path: "/"});
-        cookies.remove('nombre', {path: "/"});
-        cookies.remove('username', {path: "/"});
-        window.location.href='./';
-    }
+	cerrarSesion = () => {
+		cookies.remove('id', { path: '/' });
+		cookies.remove('primer_apellido', { path: '/' });
+		cookies.remove('segundo_apellido', { path: '/' });
+		cookies.remove('nombre', { path: '/' });
+		cookies.remove('username', { path: '/' });
+		window.location.href = './';
+	};
 
-    componentDidMount() {
-        if(!cookies.get('username')){
-            window.location.href="./";
-        }
-    }
+	componentDidMount() {
+		if (!cookies.get('username')) {
+			window.location.href = './';
+		}
+	}
 
-    render() {
-        console.log('id: '+ cookies.get('id'));
-        console.log('apellido_paterno: '+cookies.get('apellido_paterno'));
-        console.log('apellido_materno: '+cookies.get('apellido_materno'));
-        console.log('nombre: '+cookies.get('nombre'));
-        console.log('username: '+cookies.get('username'));
-        return (
-            <div>
-                Menu Principal
+	render() {
+		console.log('id: ' + cookies.get('id'));
+		console.log('primer_apellido: ' + cookies.get('primer_apellido'));
+		console.log('segundo_apellido: ' + cookies.get('segundo_apellido'));
+		console.log('nombre: ' + cookies.get('nombre'));
+		console.log('username: ' + cookies.get('username'));
+		return (
+			<>
+				<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+					<Navbar.Brand href="#home">App Login React-Bootstrap </Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link href="/menu">Perfil</Nav.Link>
+							<Nav.Link href="/menu">Pagina Principal</Nav.Link>
+							<NavDropdown title="Servicios" id="collasible-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">
+									Entretenimiento
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">
+									Informacion
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">
+									Asesorias
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">
+									Call Center
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Nav>
+							<Nav.Link href="#deets">Mas Opciones</Nav.Link>
+							<Nav.Link eventKey={2} onClick={() => this.cerrarSesion()}>
+								Cerrar Sesion
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
 
-                <br />
-                <button onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
-            </div>
-        );
-    }
+				<Card>
+					<Card.Header>
+						<Nav variant="pills" defaultActiveKey="#first">
+							<Nav.Item>
+								<div className="alert alert-light" role="alert">
+									<h3>Estas en la Seccion Perfil del Usuario</h3>
+								</div>
+							</Nav.Item>
+							{/* <Nav.Item>
+								<Nav.Link href=" ">Pagina Principal</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link href="#disabled" disabled>
+									Intranet
+								</Nav.Link>
+							</Nav.Item> */}
+						</Nav>
+					</Card.Header>
+					<Card.Body>
+                    <div className="card border-primary mb-3" styled="max-width: 18rem;">
+						<div className="col-md-6 p-3 mb-2 bg-info text-white" styled="max-width: 18rem;" >
+							<Card.Title>{'Nombre: ' + cookies.get('nombre')}</Card.Title>
+						</div>
+						<div className="col-md-6 p-3 mb-2 bg-info text-white" styled="max-width: 18rem;">
+							<Card.Title color="blue">
+								{'Primer Apellido: ' + cookies.get('primer_apellido')}
+							</Card.Title>
+						</div>
+                        <div className="col-md-6 p-3 mb-2 bg-info text-white" styled="max-width: 18rem;">
+							<Card.Title color="blue">
+								{'Segundo Apellido: ' + cookies.get('segundo_apellido')}
+							</Card.Title>
+						</div>
+						
+
+						<Card.Text><br />
+							Why do we use it? It is a long established fact that a reader will
+							be distracted by the readable content of a page when looking at
+							its layout. The point of using Lorem Ipsum is that it has a
+							more-or-less normal distribution of letters, as opposed to using
+							'Content here, content here', making it look like readable
+							English. Many desktop publishing packages and web page editors now
+							use Lorem Ipsum as their default model text, and a search for
+							'lorem ipsum' will uncover many web sites still in their infancy.
+							Various versions have evolved over the years, sometimes by
+							accident, sometimes on purpose (injected humour and the like).
+						</Card.Text>
+                        
+						<Button className="col-md-6 p-3" variant="primary" onClick={() => this.cerrarSesion()}>
+							Cerrar Sesion
+						</Button>
+                        </div>
+					</Card.Body>
+				</Card>
+			</>
+		);
+	}
 }
 
 export default Menu;
